927208044dfe2e5977a07045102170cc47f2bc4a
houdavic <houdavic123@yahoo.com>
2017-04-14 23:23:07 +0200

    Exploratory Data Analysis project 1

---
+++ plot2.R
@@ -0,0 +1,16 @@
+
+data<- read.table("household_power_consumption.txt",header = TRUE,sep=";",stringsAsFactors = FALSE,dec='.')
+
+subdata<- data[data$Date %in% c("1/2/2007","2/2/2007") , ]
+
+
+global_active_power<- as.numeric(subdata$Global_active_power)
+
+datetime<- strptime(paste(subdata$Date, subdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S ")
+
+
+png("plot2.png",width = 480 ,height = 480)
+
+plot(datetime,global_active_power,type="l",xlab = "" ,ylab = "Global Active Power (Kilowatts)")
+
+dev.off()
